5851bd22f7bd1d569e1b7dabd8ba1830570f413e
Ham kemboi <kemboiham3@gmail.com>
2023-06-29 01:42:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+];
+
+function superbowlWin(record) {
+  const win = record.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+// Usage example:
+const winYear = superbowlWin(record);
+console.log(winYear); // Output: 2015
+
